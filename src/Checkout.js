04f7993e8c706961d5bf360728ebe04b17466787
taevonjordan11 <taevonjordan1@gmail.com>
2020-12-29 14:43:57 -0500

    sub total calculator

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Checkout.css";
-import Subtotal from './Subtotal'
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -17,8 +17,7 @@ function Checkout() {
         </div>
       </div>
       <div className="checkout__right">
-          <Subtotal />
-        <h2>Sub total will go here</h2>
+        <Subtotal />
       </div>
     </div>
   );
