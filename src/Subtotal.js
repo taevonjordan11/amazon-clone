5b265a92b25b614d1447b64030bb15b0e3371db3
taevonjordan11 <taevonjordan1@gmail.com>
2020-12-30 16:59:31 -0500

    calculate total 1

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 Items): <strong>0</strong>
+              Subtotal ({basket?.length} Items): <strong>0</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
