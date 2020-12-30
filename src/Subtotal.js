e05deac4e218bb72d9803f7f8eacdc84150f3de7
taevonjordan11 <taevonjordan1@gmail.com>
2020-12-30 18:33:40 -0500

    remove items from basket

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
@@ -11,7 +12,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket?.length} Items): <strong>0</strong>
+              Subtotal ({basket?.length} Items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -19,7 +20,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSerperator={true}
         prefix={"$"}
--- src/reducer.js
+++ src/reducer.js
@@ -2,13 +2,37 @@ export const initialState = {
   basket: [],
 };
 
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
+    // pushes items to basket
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
+      };
+
+    // removes items from basket. not most efficient method
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let nBasket = [...state.basket];
+
+      if (index >= 0) {
+        nBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `cant remove item with (id: $action.id)... item not in basket?`
+        );
+      }
+
+      return {
+        ...state,
+        basket: nBasket,
       };
 
     default:
